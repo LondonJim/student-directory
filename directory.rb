7ba992bb0e03a16c1ef66299bde1b11d2827f933
James Sutherland <jrsutherland78@googlemail.com>
2018-07-30 10:35:11 +0100

    Add load list from command line

--- directory.rb
+++ directory.rb
@@ -1,41 +1,18 @@
 @students = []
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # first name to enter
-  name = gets.chomp
-  # while the name is not empty, keep repeating the code below
-  while !name.empty? do
-    # adding the student hash to the students array
-    @students << {name: name, cohort: :november}
-    puts "#{name} entered, now we have #{@students.count} students"
-    name = gets.chomp
-  end
-end
-
-def print_header
-  header = "The students of Villians Academy"
-  puts header
-  puts "-------------".center(header.length) #centre the line to 'header' variable
-end
-
-def print
-  @students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great student#{'s' if (@students.count > 1)}"
-  puts ""
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
 end
 
 def interactive_menu
   students = []
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -44,6 +21,7 @@ def process(selection)
     when "1"
       input_students
     when "2"
+      puts ""
       show_students
     when "3"
       save_students
@@ -58,19 +36,41 @@ def process(selection)
   end
 end
 
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # first name to enter
+  name = STDIN.gets.chomp
+  # while the name is not empty, keep repeating the code below
+  while !name.empty? do
+    # adding the student hash to the students array
+    @students << {name: name, cohort: :august}
+    puts "#{name} entered, now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
 end
 
 def show_students
   print_header
-  print
+  print_list
   print_footer
+end
+
+def print_header
+  header = "The Students of Villians Academy"
+  puts header
+  puts "-------------".center(header.length) #centre the line to 'header' variable
+end
+
+def print_list
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great student#{'s' if (@students.count > 1)}"
+  puts ""
 end
 
 def save_students
@@ -83,7 +83,7 @@ def save_students
   file.close
 end
 
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
@@ -91,4 +91,18 @@ def load_students
   end
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} student#{'s' if @students.count > 1} from #{filename}"
+    puts ""
+  else
+    puts "Sorry, #{filename} does not exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu

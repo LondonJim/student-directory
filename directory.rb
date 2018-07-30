87fa737bf3c54d9490cf2f6fd71033973b5acfc1
James Sutherland <jrsutherland78@googlemail.com>
2018-07-30 09:43:37 +0100

    Add gitignore and directorysave option

--- directory.rb
+++ directory.rb
@@ -34,9 +34,7 @@ end
 def interactive_menu
   students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
 
     selection = gets.chomp
     puts ""
@@ -46,6 +44,8 @@ def interactive_menu
         input_students
       when "2"
         show_students
+      when "3"
+        save_students
       when "9"
         exit
       else
@@ -57,6 +57,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -64,6 +65,16 @@ def show_students
   print_header
   print
   print_footer
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
